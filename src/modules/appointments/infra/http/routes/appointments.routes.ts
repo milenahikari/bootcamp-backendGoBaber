2c4c2fbcbbd224837955acbd7df92e9d478ab7a8
Milena <milenahikari11@gmail.com>
2020-05-28 21:20:19 -0300

    Principio Liskov

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { parseISO } from 'date-fns'; // startOfHour usa as horas inteiras, e parseISO permite trabalhar com o Date()
 import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
