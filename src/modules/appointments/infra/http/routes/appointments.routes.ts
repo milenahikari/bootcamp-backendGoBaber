bcf97b87df0886e42bc7466feb236fa73776bc30
Milena <milenahikari11@gmail.com>
2020-06-20 13:23:32 -0300

    Agenda do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import AppointmentsController from '@modules/appointments/infra/controllers/AppointmentsController';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
@@ -15,5 +17,6 @@ appointmentRouter.use(ensureAuthenticated);
 // });
 
 appointmentRouter.post('/', appointmentsController.create);
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentRouter;
---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,33 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentRepository: IAppointmentsRepository,
+  ) { }
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentRepository.findAllInDayFromProvider(
+      { provider_id, day, month, year },
+    );
+
+    return appointments;
+  }
+}
+export default ListProviderAppointmentsService;
