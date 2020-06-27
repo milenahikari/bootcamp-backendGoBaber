b54659a2a9ebc6427bfaf0e92abf0d02aa8c8c16
Milena <milenahikari11@gmail.com>
2020-06-27 15:21:51 -0300

    Ajustes nos dias disponiveis para marcar um appointment

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -43,6 +43,8 @@ class ListProviderMonthAvailabilityService {
 
     // Percorre todos os dias do mês
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       // Percorre cada appoinment do mês e verifica se tem um appointment agendado nesse dia
       const appointmentInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
@@ -50,7 +52,8 @@ class ListProviderMonthAvailabilityService {
 
       return {
         day,
-        available: appointmentInDay.length < 10, // 1 agendamento por hora, das 8h as 17h só pode existir em um dia 10 agendamentos no máximo
+        available:
+          isAfter(compareDate, new Date()) && appointmentInDay.length < 10, // 1 agendamento por hora, das 8h as 17h só pode existir em um dia 10 agendamentos no máximo
       };
     });
 
